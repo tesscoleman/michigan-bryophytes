fbb2c18d79a3714b8fa3b511f4ffe153a37b1fc2
tessc <colem391@msu.edu>
2024-01-14 17:40:37 -0500

    added hash router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom";
 import 'react-photo-view/dist/react-photo-view.css';
 
 const root = ReactDOM.createRoot(
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
